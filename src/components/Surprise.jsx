4dc06a65e6c135a9c06023e39c45e5c8503701eb
Pranay-4748 <pranaychimankar4748@gmail.com>
2025-02-14 16:37:17 +0530

    last message

--- src/components/Surprise.jsx
+++ src/components/Surprise.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Balloons from "../Ballon";
-import sound from "/music.mp3";
 import Candle from "../Candle";
 import { useNavigate } from "react-router-dom";
 import cake from "../assets/cake.png"
