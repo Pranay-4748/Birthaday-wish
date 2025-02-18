001521d4dff6aa9ee6ef75e30069560c06d13fca
Pranay-4748 <pranaychimankar4748@gmail.com>
2025-02-18 22:45:09 +0530

    route

--- src/components/Gift.jsx
+++ src/components/Gift.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import Bg from "../Background";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Gift() {
   return (
     <div className="flex w-full items-center justify-center">
-      <Bg/>
+      <Bg />
       <div className="item text-center absolute h-40 w-3 flex flex-col items-center justify-center">
-        <h2 className="text-2xl">Once again Happy Birthday Prachi </h2>
+        <h2 className="text-2xl">Once again Happy Birthday Prachi</h2>
         <br />
         <p>
           Believe in yourself and all that you are because you are capable of
           amazing things.
-          <br />Sending you warm birthday wishes on your special day I
-          have made something for u 
+          <br />
+          Sending you warm birthday wishes on your special day. I have made
+          something for you.
           <br />
           <br />
-          <p>Click Below Link To See</p>
-          <a href="http://birthdaywishp.netlify.app/surprise">http://birthdaywishp.netlify.app/surprise</a>
         </p>
+        <Link to="/surprise">
+          <motion.button
+            type="button"
+            className="dbtn rounded-full absolute z-10"
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            whileHover={{ outline: "1px solid #535bf2" }}
+            whileTap={{ scale: 0.99 }}
+          >
+            Click Here
+          </motion.button>
+        </Link>
       </div>
     </div>
   );
